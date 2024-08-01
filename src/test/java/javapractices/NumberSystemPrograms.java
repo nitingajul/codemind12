dccc4e851146f23f25e8c8b1e06f43478c00b650
tulshidast <42244862+tulshidast@users.noreply.github.com>
2024-08-01 07:36:26 +0530

    Update NumberSystemPrograms.java

--- src/test/java/javapractices/NumberSystemPrograms.java
+++ src/test/java/javapractices/NumberSystemPrograms.java
@@ -5,7 +5,7 @@ public class NumberSystemPrograms {
 	public static void main(String[] args) {
 
 		// 2*1 ,2*2,2*3
-		// 3*1...
+		// 3*1......
 
 		for (int i = 2; i <= 10; i++) {
 			for (int j = 1; j <= 10; j++) {
